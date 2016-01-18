05015d0e0590f530e22fd53c8291d2aa732f80ca
Misha Khohlov <Misha.Khohlov@yandex.ua>
2016-01-18 01:58:04 +0000

    18.01 1:57

--- builds/dist/scroll.js
+++ builds/dist/scroll.js
@@ -1,22 +1,34 @@
-$(document).ready(begin);
+$(window).load(begin);
 function begin() {
-	$('a[href^="#"]').click(function(){
-	var target = $(this).attr('href');
-	$('html, body').animate({scrollTop: $(target).offset().top}, 800);
-	return false;
-	});
-	
+//	$('a[href^="#"]').click(function(){
+//	var target = $(this).attr('href');
+//	$('html, body').animate({scrollTop: $(target).offset().top}, 800);
+//	return false;
+//	});
+	setTimeout(readyOn, 20);
+	function readyOn() {
+		$('.header_link_home').hover(function(event){
+			console.log(event.currentTarget.className)
+			
+		})
+	}
 	$(window).scroll(function(eventObject){
 		var scroll = Math.round($(window).scrollTop());
-		$('.header').css("top", scroll+50+'px');
+		//$('.header').css("top", scroll +'px');
+		// $(window).scrollTop(673);
 		if(scroll>0) {
-			$('.header').addClass('headerShadow');	
+			if(!$('.header').hasClass('headerMin')) {
+				$('.header').addClass('headerMin');
+				$('.header_logo').addClass('header_logoMin');
+			}
 		} else {
-			$('.header').removeClass('headerShadow');
+			if($('.header').hasClass('headerMin')) {
+				$('.header').removeClass('headerMin');
+			$('.header_logo').removeClass('header_logoMin');
+			}
 		}
 	});
 }
-
 //$('a[href*=#]').bind("click", function(e){
 //var anchor = $(this);
 //$('html, body').stop().animate({
